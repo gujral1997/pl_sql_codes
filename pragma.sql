983b42a32f469a42dc6ee9aedd22a88190e7012b
Ansh Gujral <anshgujral1997@gmail.com>
2017-12-14 20:19:14 +0530

    added pragma file

---
+++ pragma.sql
@@ -0,0 +1,10 @@
+set serveroutput on;
+declare
+large_value exception;
+pragma exception_init(large_value,-01438);
+begin
+insert into sp values(10000000000000000000000000000000000000,1000000000,100000);
+	exception
+	when large_value then
+	dbms_output.put_line('value too big');
+	end;
